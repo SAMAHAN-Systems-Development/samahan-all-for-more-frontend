95c2a1ac8a22d1ba4ee3f5174682b810b1f8c900
edgee20 <ejfusingan0@gmail.com>
2024-09-01 03:36:39 +0800

    feat: added a new section with SMHN logo, email, SMHN website, and addusamahan soc med handle. also imported elements in /assets/images and updated the design of the branding section

--- src/components/AboutPage.tsx
+++ src/components/AboutPage.tsx
@@ -2,31 +2,98 @@ import React from 'react';
 
 const AboutPage = () => {
   return (
-    <main className="p-12">
-      
-      <section>
-        <div>
-          <img src="/assets/images/Property 1=WHITE.png" alt="" />
-          <p className='text-blue mb-14 leading-tight text-center'>The Samahan ng Mga Mag-Aaral ng Pamantasang Ateneo de Davao of SAMAHAN in the lone autonomous Student Government of the College unit of Ateneo de Davao University</p>
+    <main className="p-12 overflow-x-hidden">
+      <section className="flex items-center flex-col mb-72">
+        <div className="flex items-center flex-col mx-[24rem] mb-10">
+          <img
+            src="/assets/images/Property 1=WHITE.png"
+            alt="Samahan White Logo"
+            className="w-[36rem] h-auto"
+          />
+          <p className="text-blue leading-tight text-center">
+            The Samahan ng Mga Mag-Aaral ng Pamantasang Ateneo de Davao, or
+            SAMAHAN, is the lone autonomous Student Government of the College
+            unit of Ateneo de Davao University
+          </p>
+        </div>
+
+        <div className="flex items-center">
+          <div className="text-blue text-xs mb-14 flex items-center mr-6">
+            <div className="w-4 h-4 bg-blue rounded-full mr-2 flex items-center justify-center">
+              <img src="/assets/images/Email Vector.png" alt="Email Vector" />
+            </div>
+            <p>
+              <a href="mailto:samahan@addu.edu.ph" className="hover:underline">
+                samahan@addu.edu.ph
+              </a>
+            </p>
+          </div>
+          <div className="text-blue text-xs mb-14 flex items-center mr-6">
+            <div className="w-4 h-4 bg-blue rounded-full mr-2 flex items-center justify-center">
+              <img
+                src="/assets/images/Website Vector.png"
+                alt="Website Vector"
+              />
+            </div>
+            <p>
+              <a href="" className="hover:underline">
+                samahan.addu.edu.ph
+              </a>
+            </p>
+          </div>
+          <div className="text-blue text-xs mb-14 flex items-center mr-6">
+            <div className="w-4 h-4 bg-blue rounded-full mr-2 flex items-center justify-center">
+              <img src="/assets/images/@ Vector.png" alt="@ Vector" />
+            </div>
+            <p>
+              <a href="" className="hover:underline">
+                addusamahan
+              </a>
+            </p>
+          </div>
         </div>
       </section>
-      <article>
-        <div className="flex items-center">
-          {/*waiting for the logo for*/}
-          <img
-            src="/assets/images/Layer 1.png"
-            alt="Samahan White Logo"
-            className="w-36 h-auto "
-          />
-          <img
-            src="/assets/images/A4M LOGO.png"
-            alt="A4M Logo"
-            className="w-48 h-auto"
-          />
+
+      <article className="flex relative">
+        <div>
+          <div>
+            <img
+              src="/assets/images/Blue SMHN Logo Normal.png"
+              alt="Left Image 1"
+              className="w-[20rem] h-auto absolute top-0 left-[-10rem] rotate-[30deg]"
+            />
+          </div>
+          <div className="">
+            <img
+              src="/assets/images/Blue E1.png"
+              alt="Left Image 2"
+              className="w-28 h-auto absolute left-12 top-1/2 transform -translate-y-1/2 rotate-[-30deg]"
+            />
+          </div>
+          <div className="">
+            <img
+              src="/assets/images/Blue O3.png"
+              alt="Left Image 3"
+              className="w-[16rem] h-auto absolute bottom-0 left-[-10rem] rotate-[-150deg]"
+            />
+          </div>
         </div>
 
         {/*to align this whole section with the logo*/}
-        <section className="mx-6">
+        <section className="mx-[15rem]">
+          <div className="flex items-center mb-7 mt-28">
+            {/*waiting for the logo for*/}
+            <img
+              src="/assets/images/A4M S-LOGO.png"
+              alt=""
+              className="w-28 h-auto "
+            />
+            <img
+              src="/assets/images/A4M LOGO.png"
+              alt="A4M Logo"
+              className="w-48 h-auto"
+            />
+          </div>
           <h1 className="text-blue font-bold text-7xl mb-14">THE BRANDING</h1>
           <p className="text-blue mb-14 leading-tight">
             In envisioning Our Ateneo, we also aim to achieve a SAMAHAN that
@@ -55,7 +122,7 @@ const AboutPage = () => {
             SAMAHAN that personifies these virtues, epitomizing excellence in
             all that we do, while putting the students at our topmost priority.
           </p>
-          <p className="text-blue mb-14 leading-tight">
+          <p className="text-blue leading-tight mb-44">
             This time, we establish SAMAHAN not just a student-body but as a
             community that supports and uplifts one another. All for More
             reflects this spirit of liberation and inclusivity, striving to
@@ -64,6 +131,29 @@ const AboutPage = () => {
             create illuminating milestones-All for More.
           </p>
         </section>
+        <div>
+          <div>
+            <img
+              src="/assets/images/Blue M1.png"
+              alt="Left Image 1"
+              className="w-[19rem] h-auto absolute top-28 right-[-10rem] rotate-[-30deg]"
+            />
+          </div>
+          <div className="">
+            <img
+              src="/assets/images/Blue R1.png"
+              alt="Left Image 2"
+              className="w-24 h-auto absolute right-12 top-[60%] transform -translate-y-1/2 rotate-[-30deg]"
+            />
+          </div>
+          <div className="">
+            <img
+              src="/assets/images/Blue E1.png"
+              alt="Left Image 3"
+              className="w-[13rem] h-auto absolute bottom-0 right-[-6rem] rotate-[30deg]"
+            />
+          </div>
+        </div>
       </article>
     </main>
   );
