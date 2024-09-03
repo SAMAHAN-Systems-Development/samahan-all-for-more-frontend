f6ddf4647a1873b416f53abfa3fc2c71b7321301
anya <txcokun@gmail.com>
2024-09-03 20:13:35 +0800

    feat: add intial contact information with icons

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,15 @@
+import HeroSection from '@/components/HeroSection/HeroSection';
 import type { Metadata } from 'next';
 
 export const metadata: Metadata = {
   title: 'Samahan 2024',
-  description:
-    'The official Samahan 2024 Website.',
+  description: 'The official Samahan 2024 Website.',
 };
 
 export default function Home() {
   return (
     <main>
-      {/* add sections here */}
+      <HeroSection />
     </main>
   );
 }
--- src/components/HeroSection/HeroSection.tsx
+++ src/components/HeroSection/HeroSection.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import logo from '@/stories/assets/hero/A4M LOGO.png';
-import Image from 'next/image';
+import Image, { StaticImageData } from 'next/image';
 import email_icon from '@/stories/assets/hero/email_icon.png';
 import website_icon from '@/stories/assets/hero/website_icon.png';
 import sns_icon from '@/stories/assets/hero/sns_icon.png';
@@ -8,8 +8,16 @@ import edge from '@/stories/assets/hero/round end.svg';
 import BallFrame from '../ui/BallFrame';
 
 export default function HeroSection() {
+  const socials: { icon: StaticImageData; title: string; value: string }[] = [
+    { icon: email_icon, title: 'email', value: 'samahan@addu.edu.ph' },
+    { icon: website_icon, title: 'website', value: 'samahan.addu.edu.ph' },
+    { icon: sns_icon, title: 'sns', value: 'addusamahan' },
+  ];
+
   return (
-    <section className="font-inter h-[80vh] w-full relative max-w-[1440px]">
+    <section
+      className={` h-[80vh] w-full relative max-w-[1440px] tracking-wider`}
+    >
       <div className="w-full h-full bg-blue flex flex-col items-center justify-center text-white">
         {/* temp navigation bar */}
         <div className="w-9/12 mx-auto rounded-full bg-white h-14 sticky top-10 z-10 " />
@@ -25,17 +33,26 @@ export default function HeroSection() {
             />
           </div>
           <span className="leading-tight text-center">
-            <h2 className="text-base">
+            <h2 className="text-base font-medium" style={{ fontWeight: 600 }}>
               SAMAHAN NG MGA MAG-AARAL NG PAMANTASANG ATENEO DE DAVAO
             </h2>
-            <h3 className="font-light text-xs">
+            <h3 className="font-thin text-xs" style={{ fontWeight: 400 }}>
               The Student Government of the College Unit of Ateneo de Davao
               University
             </h3>
           </span>
 
           {/* socials */}
-          <div className="flex justify-between"></div>
+          <div className="flex justify-between">
+            {socials.map((social) => (
+              <div className="flex gap-2 text-xs p-2 items-center">
+                <div className="rounded-full h-5 w-5 bg-white p-1 flex justify-center items-center overflow-visible">
+                  <Image src={social.icon} alt={social.title} />
+                </div>
+                <span>{social.value}</span>
+              </div>
+            ))}
+          </div>
         </div>
       </div>
 
