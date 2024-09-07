9dacf877ef488e35ded4a8841a0dd2fed9b2c7d3
anya <txcokun@gmail.com>
2024-09-07 15:37:30 +0800

    feat: initial spring animation for left spheres

--- src/components/ui/BallFrame.tsx
+++ src/components/ui/BallFrame.tsx
@@ -4,35 +4,51 @@ import o_ball from '@/stories/assets/hero/O.png';
 import r_ball from '@/stories/assets/hero/R.png';
 import e_ball from '@/stories/assets/hero/E.png';
 import Ball from '@/components/ui/Balls';
+import { motion } from 'framer-motion';
 
 export default function BallFrame() {
   return (
     <>
-      {/* left spheres */}
-      <div className="h-[670px] w-1/2 absolute transform translate-x-[-8rem] bottom-0 z-10 overflow-visible">
-        <Ball style="top-10 left-10 w-32" letter={o_ball} />
-        <Ball style="top-32 left-44 w-10 -rotate-12" letter={m_ball} />
-        <Ball style="bottom-64 left-0 w-56 rotate-45" letter={r_ball} />
-        <Ball style="bottom-80 left-64 w-24 -rotate-45" letter={e_ball} />
-        <Ball style="bottom-32 left-28 w-40" letter={o_ball} />
-        <Ball style="bottom-10 left-64 w-64 rotate-12" letter={m_ball} />
-        <Ball style="bottom-24 right-[6.5rem] w-24 rotate-45" letter={r_ball} />
-      </div>
-      {/* right spheres */}
-      <div className="h-[670px] w-1/2 absolute transform translate-x-[8rem] bottom-0 right-0 z-10 overflow-visible">
-        <Ball style="top-10 right-4 w-56 -rotate-45" letter={m_ball} />
-        <Ball style="top-32 right-72 w-12 -rotate-45" letter={r_ball} />
-        <Ball style="bottom-80 right-64 w-24" letter={o_ball} />
-        <Ball
-          style="bottom-56 right-20 w-40 -rotate-[145deg]"
-          letter={e_ball}
-        />
-        <Ball
-          style="bottom-10 right-40 w-64 -rotate-[110deg]"
-          letter={r_ball}
-        />
-        <Ball style="bottom-64 left-64 w-12" letter={e_ball} />
-        <Ball style="bottom-5 left-24 w-56 rotate-[100deg]" letter={o_ball} />
+      <div className="w-[1440px]">
+        {/* left spheres */}
+        <motion.div className="h-[670px] w-[720px] border absolute origin-top-left transform -translate-x-32 bottom-0 z-10 overflow-visible">
+          <Ball style="w-32" top={48} left={40} letter={o_ball} />
+          <Ball style="w-10 -rotate-12" top={128} left={176} letter={m_ball} />
+          <Ball style="w-56 rotate-45" bottom={256} left={0} letter={r_ball} />
+          <Ball
+            style="w-24 -rotate-45"
+            bottom={320}
+            left={256}
+            letter={e_ball}
+          />
+          <Ball style="w-40" bottom={128} left={112} letter={o_ball} />
+          <Ball
+            style="w-64 rotate-12"
+            bottom={40}
+            right={208}
+            letter={m_ball}
+          />
+          <Ball
+            style="bottom-24 right-[6.5rem] w-24 rotate-45"
+            letter={r_ball}
+          />
+        </motion.div>
+        {/* right spheres */}
+        {/* <div className="h-[670px] w-[720px] absolute transform translate-x-[8rem] bottom-0 right-0 z-10 overflow-visible">
+          <Ball style="top-10 right-4 w-56 -rotate-45" letter={m_ball} />
+          <Ball style="top-32 right-72 w-12 -rotate-45" letter={r_ball} />
+          <Ball style="bottom-80 right-64 w-24" letter={o_ball} />
+          <Ball
+            style="bottom-56 right-20 w-40 -rotate-[145deg]"
+            letter={e_ball}
+          />
+          <Ball
+            style="bottom-10 right-40 w-64 -rotate-[110deg]"
+            letter={r_ball}
+          />
+          <Ball style="bottom-64 left-64 w-12" letter={e_ball} />
+          <Ball style="bottom-5 left-24 w-56 rotate-[100deg]" letter={o_ball} />
+        </div> */}
       </div>
     </>
   );
--- src/components/ui/Balls.tsx
+++ src/components/ui/Balls.tsx
@@ -1,12 +1,55 @@
 import React from 'react';
 import Image, { StaticImageData } from 'next/image';
+import { motion } from 'framer-motion';
 
-const Ball = (props: { style: string; letter: StaticImageData }) => {
+const Ball = (props: {
+  style: string;
+  letter: StaticImageData;
+  top?: number;
+  bottom?: number;
+  left?: number;
+  right?: number;
+}) => {
   return (
     <>
-      <div className={`absolute ${props.style} z-10`}>
-        <Image src={props.letter} alt="ball" className="object-contain" />
-      </div>
+      <motion.div
+        className={`absolute ${props.style} z-10 origin-top-left`}
+        initial={{
+          rotate: 12,
+          scale: 1.5,
+          translateY: 200,
+          translateX: -200,
+          top: props.top! * 0.5,
+          bottom: props.bottom! * 0.5,
+          left: props.left! * 0.5,
+          right: props.right! * 0.15,
+        }}
+        animate={{
+          rotate: 0,
+          scale: 1,
+          translateY: 0,
+          translateX: 0,
+          top: props.top,
+          bottom: props.bottom,
+          left: props.left,
+          right: props.right,
+        }}
+        transition={{
+          type: 'spring',
+          duration: 2,
+          mass: 1,
+          stiffness: 28.44,
+          damping: 8,
+          delay: 0.15,
+        }}
+      >
+        <Image
+          src={props.letter}
+          alt="ball"
+          className="object-contain"
+          priority
+        />
+      </motion.div>
     </>
   );
 };
