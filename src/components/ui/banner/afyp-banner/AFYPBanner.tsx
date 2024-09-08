8c538094d4b12c8851544be0f048d8e8bc81032a
LoejeeMan <72646329+GuyWithBag@users.noreply.github.com>
2024-09-08 17:35:25 +0800

    feature/Added and styled subtitle/main and subheader/footer-main for AFYPBanner and added a stories file for it as well

--- src/components/ui/banner/afyp-banner/AFYPBanner.tsx
+++ src/components/ui/banner/afyp-banner/AFYPBanner.tsx
@@ -1,32 +1,29 @@
 import Image from 'next/image';
 import React from 'react';
 import BannerGradient from '../banner-gradient/BannerGradient';
+import { BannerImage } from '../banner-image/BannerImage';
 
-export interface Props {
-  title?: string;
+interface Props extends React.HTMLAttributes<HTMLDivElement> {
   subtitle?: string;
+  subheader?: string;
 }
 
-export const SAMAHANMembersBanner: React.FC<Props> = ({
-  title,
+export const AFYPBanner: React.FC<Props> = ({
   subtitle,
-}: Props) => {
+  subheader,
+  ...props
+}) => {
   return (
-    <div className="absolute bg-blue w-full h-full">
-      <BannerGradient />
-      <div className=" flex justify-center items-center absolute w-[100%] h-[100%]">
-        <p className="text-title text-white text-center">
-          <h2 className=" ">{subtitle}</h2>
-          <h1 className="  font-bold ">{title}</h1>
+    <BannerImage
+      className="flex items-center justify-center bg-blue"
+      {...props}
+    >
+      <div>
+        <p className=" text-white text-center ">
+          <h4 className=" text-main font-bold ">{subtitle}</h4>
+          <h5 className=" text-footer-main">{subheader}</h5>
         </p>
       </div>
-      <Image
-        alt="SAMAHANN Members"
-        src={'/images/samahan-members.png'}
-        width={0}
-        height={0}
-        className="w-full h-full object-cover pb-14"
-      />
-    </div>
+    </BannerImage>
   );
 };
