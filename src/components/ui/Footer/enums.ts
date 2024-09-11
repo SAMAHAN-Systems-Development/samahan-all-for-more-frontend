0a65d7df49001924fb53296f4e25913a44206c91
seanchvz <seann.chvz@gmail.com>
2024-09-11 08:08:06 +0800

    fix: resolve ESLint warnings

--- src/components/ui/Footer/enums.ts
+++ src/components/ui/Footer/enums.ts
@@ -3,19 +3,19 @@ export enum SOCIAL_ICON_SIZE_ENUM {
     SMALL = 'small',
 }
 
-export enum LINK_SIZE_ENUM {
-    MAIN = 'text-main',
-    MEDIUM = 'medium',
-    REGULAR = 'text-footer-main',
-    SMALL = 'small',
-}
-
 export enum LINK_WEIGHT_ENUM {
     BOLD = 'bold',
     NORMAL = 'normal',
     SUPER_THIN = 'superThin',
     THIN = 'thin',
 
+}
+
+export enum LINK_SIZE_ENUM {
+    MAIN = 'text-main',
+    MEDIUM = 'medium',
+    REGULAR = 'text-footer-main',
+    SMALL = 'small',
 }
 
 export enum FOOTER_SIZE_ENUM {
