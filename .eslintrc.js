8f3659dc6af71bb055eb5360601aac6434b7a538
Ninya <mnamparaiso@addu.edu.ph>
2024-09-15 16:04:12 +0800

    feat: creates a folder with a json file consist of accordion data and dynamically rendered it.

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,10 +3,24 @@
  */
 module.exports = {
   ignorePatterns: ['.eslintrc.js', '*.config.js'],
+
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    project: './tsconfig.json',
+    sourceType: 'module',
+  },
+
   extends: [
     '@cyrilolanolan/ts',
     'next',
     'plugin:storybook/recommended',
-    'plugin:storybook/recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:@typescript-eslint/recommended-requiring-type-checking',
   ],
+
+  plugins: ['@typescript-eslint'],
+
+  rules: {
+    '@typescript-eslint/no-unnecessary-condition': 'error',
+  },
 };
