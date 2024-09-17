9a0458bc613bbeac06fe3dd76eccf36af9c63e9c
anya <txcokun@gmail.com>
2024-09-17 20:43:19 +0800

    fix: removed ball frame testing on page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,10 @@
 import type { Metadata } from 'next';
 
-import BallFrame from '@/components/ui/BallFrame';
-
 export const metadata: Metadata = {
   title: 'Samahan 2024',
   description: 'The official Samahan 2024 Website.',
 };
 
 export default function Home() {
-  return (
-    <main>
-      {' '}
-      <div>
-        <BallFrame />
-      </div>
-    </main>
-  );
+  return <main> test{/* add sections here */}</main>;
 }
