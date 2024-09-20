13facc513ae7bca45fb17d57ac6922ee1b6c8427
anya <txcokun@gmail.com>
2024-09-20 20:52:34 +0800

    chore: relocate Hero Section stories file

--- src/stories/components/Sections/HeroSection/HeroSection.stories.ts
+++ src/stories/components/Sections/HeroSection/HeroSection.stories.ts
@@ -1,4 +1,4 @@
-import HeroSection from '@/components/HeroSection/HeroSection';
+import HeroSection from '@/components/Sections/HeroSection/HeroSection';
 
 export default {
   title: 'HeroSection',
