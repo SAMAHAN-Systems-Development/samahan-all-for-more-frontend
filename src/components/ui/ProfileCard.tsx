3de039c13abd5fd0c5461cefc3d3f5df28b59f9e
yashiyoshi <93021275+yashiyoshi@users.noreply.github.com>
2024-09-24 20:01:06 +0800

    feat: ProfileCard implementation

---
+++ src/components/ui/ProfileCard.tsx
@@ -0,0 +1,40 @@
+import Image from "next/image";
+
+interface ProfileCardProps {
+  fullName: string;
+  position: string;
+  fileName: string;
+}
+
+const ProfileCard = ({ fullName, position, fileName }: ProfileCardProps) => {
+  // Log the props to verify data is being passed correctly
+  console.log('ProfileCard Props:', { fullName, position, fileName });
+
+  return (
+    <div className="relative w-[250px] h-[380px] text-blue hover:scale-105 transition ease-in-out delay-100">
+        
+      <div className="relative">
+        {/* Blue Shape */}
+        <div className="absolute bg-blue rounded-[32.5px] w-[250px] h-[250px] translate-y-16" />
+
+        <div className="rounded-[32px] translate-y-[4px] overflow-hidden">
+          <Image
+            src={`/images/samahan-cutouts/${fileName}`}
+            alt={`${fullName}'s Picture`}
+            width={250}
+            height={380}
+            className="relative"
+          />
+        </div>
+      </div>
+
+      {/* text section */}
+      <div className="mt-4">
+        <p className="text-[18px] font-[700] leading-[21.78px]">{fullName}</p>
+        <p className="text-[12px] font-[400]">{position}</p>
+      </div>
+    </div>
+  );
+};
+
+export default ProfileCard;
