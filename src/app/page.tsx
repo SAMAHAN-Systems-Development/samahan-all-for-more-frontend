478d0d3797da5650148e99031edd9444e57f732e
unknown <paololuisramirez@gmail.com>
2024-09-25 21:08:42 +0800

    fix: fixed the overflow on the right side of the banner

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,22 +1,10 @@
 import type { Metadata } from 'next';
 
-import { Banner } from '@/components/ui/banner/Banner';
-
 export const metadata: Metadata = {
   title: 'Samahan 2024',
   description: 'The official Samahan 2024 Website.',
 };
 
 export default function Home() {
-  return (
-    <main>
-      {/* add sections here */}
-      <Banner
-        image={'/images/samahan-members.png'}
-        imageAlt={'Samahan Members'}
-        subtitle={'Samahan'}
-        title={'Privacy Policy'}
-      />
-    </main>
-  );
+  return <main>test{/* add sections here */}</main>;
 }
