dc0895edf83e51431d4c5ae73474e9116302ca8c
unknown <paololuisramirez@gmail.com>
2024-09-28 14:35:29 +0800

    feat: added about-us folder and its page file

---
+++ src/app/about-us/page.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import AboutPage from '@/components/about-page/AboutPage';
+
+const AboutUs = () => {
+  return (
+    <div>
+      <AboutPage />
+    </div>
+  );
+};
+
+export default AboutUs;
