6523743c7a021daa3dda4b5c612fde7ee8b5248c
Ninya <mnamparaiso@addu.edu.ph>
2024-10-07 23:10:49 +0800

    Style: change in the height responsiveness of the image.

--- src/components/ui/Advocasix Modal/AdvocasixModal.tsx
+++ src/components/ui/Advocasix Modal/AdvocasixModal.tsx
@@ -5,36 +5,32 @@ import { AiOutlineClose } from 'react-icons/ai';
 interface AdvocasixModalProps {
   content: string;
   image: string;
-  onClose: () => void;
-  title: string; // Add onClose prop for closing the modal
+  title: string;
 }
 
 const AdvocasixModal: React.FC<AdvocasixModalProps> = ({
   title,
   content,
   image,
-  onClose,
 }) => {
   return (
     <div className="relative shadow-2xl w-full max-w-[900px] rounded-3xl overflow-hidden mx-auto bg-white">
       {/* Header with Cover Image */}
-      <div className="relative h-48 sm:h-56 md:h-64">
+      <div className="relative h-[400px] sm:h-[300px] md:h-[400px]">
         <img
           src={`/images/advocasix-pictures/${image}`}
           alt={title}
           className="w-full h-full object-cover"
         />
+
         {/* Close button inside modal */}
-        <button
-          onClick={onClose}
-          className="absolute top-4 right-4 text-white text-3xl"
-        >
+        <button className="absolute top-4 right-4 text-white text-3xl">
           <AiOutlineClose />
         </button>
       </div>
 
       {/* Main Content */}
-      <div className="p-6 text-blue text-center mb-44">
+      <div className="p-6 text-blue text-center mb-24">
         <h2 className="text-2xl sm:text-3xl font-bold mb-4">{title}</h2>
         <p className=" mx-6 text-sm sm:text-base">{content}</p>
       </div>
