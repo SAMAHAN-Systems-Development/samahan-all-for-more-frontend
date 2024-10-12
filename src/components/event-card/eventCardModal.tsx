d1cd6715a8aa35a1b03ee2c60bea72212e2bdb9b
Ninya <mnamparaiso@addu.edu.ph>
2024-10-12 17:02:45 +0800

    style: fix the responsiveness of the modal.

--- src/components/event-card/eventCardModal.tsx
+++ src/components/event-card/eventCardModal.tsx
@@ -28,9 +28,9 @@ const EventCardModal = ({ event, onClose }: EventCardModalProps) => {
   const { image_url } = posters;
 
   return (
-    <div className="relative shadow-2xl w-full max-w-[1000px] border-blue border-solid border-[2px] rounded-3xl overflow-hidden mx-auto bg-white">
+    <div className="relative shadow-2xl w-full max-w-[90%] lg:max-w-[1000px] border-blue border-solid border-[2px] rounded-3xl overflow-hidden mx-auto bg-white">
       {/* Header with Cover Image */}
-      <div className="relative h-[300px] ">
+      <div className="relative h-[200px] sm:h-[250px] md:h-[300px] lg:h-[350px]">
         <Image
           src={image_url}
           alt={name}
@@ -49,30 +49,38 @@ const EventCardModal = ({ event, onClose }: EventCardModalProps) => {
       </div>
 
       {/* Main Content */}
-      <div className="p-6 text-blue">
-        <h2 className="text-2xl sm:text-3xl font-bold mb-4">{name}</h2>
-        <div className="flex flex-row gap-x-5">
-          <span className="flex flex-row gap-x-2 justify-center items-center">
-            <PiUsersFill size={25} />
-            open to all AdDU Students
+      <div className="p-4 sm:p-6 text-blue">
+        <h2 className="text-xl sm:text-2xl lg:text-3xl font-bold mb-5 sm:mb-4">
+          {name}
+        </h2>
+        <div className="flex flex-col sm:flex-row gap-y-4 gap-x-5">
+          <span className="flex items-center gap-x-2">
+            <PiUsersFill className="text-xl sm:text-2xl md:text-3xl lg:text-2xl" />
+            <span className="text-sm sm:text-base">
+              Open to all AdDU Students
+            </span>
           </span>
 
-          <span className="flex flex-row gap-x-2 justify-center items-center">
-            <BsCalendarWeek size={25} />
-            {start_time} -<span>{end_time}</span>
+          <span className="flex items-center gap-x-2">
+            <BsCalendarWeek className="text-xl sm:text-2xl md:text-3xl lg:text-2xl" />
+            <span className="text-sm sm:text-base">
+              {start_time} - <span>{end_time}</span>
+            </span>
           </span>
-          <span className="flex flex-row justify-center items-center">
-            <MdLocationOn size={25} />
-            {location.name}
+
+          <span className="flex items-center gap-x-2">
+            <MdLocationOn className="text-xl sm:text-2xl md:text-3xl lg:text-2xl" />
+            <span className="text-sm sm:text-base">{location.name}</span>
           </span>
         </div>
-        <p className="text-base mt-3">{description}</p>
+        <p className="text-sm sm:text-base mt-5">{description}</p>
       </div>
 
-      <div className="flex flex-col p-6 text-blue italic">
-        <span className="">Dept. Name</span>
-        <span>deptname@addu.edu.ph</span>
-        <Link href={registration_link} className="mt-8">
+      {/* Footer Content */}
+      <div className="flex flex-col p-4 sm:p-6 text-blue italic">
+        <span className="text-sm sm:text-base">Dept. Name</span>
+        <span className="text-sm sm:text-base">deptname@addu.edu.ph</span>
+        <Link href={registration_link} className="mt-6 sm:mt-8">
           <Button size={'wide'} text="Register Now" />
         </Link>
       </div>
