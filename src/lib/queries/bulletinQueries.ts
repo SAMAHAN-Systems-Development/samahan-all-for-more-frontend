2d29a83de40f205897da649482dbe6a5b3327303
Miggy Alino <miggyalino35@gmail.com>
2024-10-16 21:51:13 +0800

    fixed merge conflicts

--- src/lib/queries/bulletinQueries.ts
+++ src/lib/queries/bulletinQueries.ts
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import type { UseQueryResult } from '@tanstack/react-query';
 import { useQuery } from '@tanstack/react-query';
 import { getBulletins } from '@/lib/services/bulletinService';
@@ -10,16 +9,5 @@ export const useGetBulletins = (
   return useQuery({
     queryKey: ['bulletins', { ...getBulletinData }],
     queryFn: () => getBulletins(getBulletinData),
-=======
-import { useQuery } from '@tanstack/react-query';
-
-import { getBulletin } from '@/lib/services/bulletinService';
-import type { Bulletin } from '@/lib/types/bulletin.type';
-
-export const useBulletins = () => {
-  return useQuery<Bulletin[], Error>({
-    queryKey: ['bulletins'],
-    queryFn: getBulletin,
->>>>>>> 154a827b227f5aaf458540aaf8c8a3d85fcf530e
   });
 };
--- src/lib/services/bulletinService.ts
+++ src/lib/services/bulletinService.ts
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import { _delete, _get, _post, _put } from '@/lib/client/client';
 import { GetBulletinDto } from '../types/dto/getBulletinData.type';
 
@@ -38,17 +37,3 @@ const deleteBulletin = async (id: number): Promise<void> => {
 };
 
 export { getBulletins, createBulletin, updateBulletin, deleteBulletin };
-=======
-import { _get } from '@/lib/client/client';
-import type { Bulletin } from '@/lib/types/bulletin.type';
-
-export const getBulletin = async (): Promise<Bulletin[]> => {
-  const response = await _get<Bulletin[]>('/api/bulletins');
-  return response.data;
-};
-
-export const getPdfAttachmmentId = async (id: number): Promise<Bulletin[]> => {
-  const response = await _get<Bulletin[]>(`/api/bulletins/${id}`);
-  return response.data;
-};
->>>>>>> 154a827b227f5aaf458540aaf8c8a3d85fcf530e
