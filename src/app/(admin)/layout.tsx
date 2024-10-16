b25d3bfcde6e434dde8957e098bfbb3aaf1ea31f
Miggy Alino <miggyalino35@gmail.com>
2024-10-16 22:18:26 +0800

    fixed merge conflicts

--- src/app/(admin)/layout.tsx
+++ src/app/(admin)/layout.tsx
@@ -10,11 +10,6 @@ export default function AdminLoginLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-  const { data: user, isFetched } = useGetUser();
-  if (!user) {
-    redirect('/login');
-  }
-
   return (
     <section className="h-screen w-full flex">
       <Sidebar />
