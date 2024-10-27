7d910b5500231da31967a9f9e1a3255a4523edcb
Mariel <marmabs692@gmail.com>
2024-10-27 17:51:55 +0800

    refactor: pulled updated main

---
+++ src/components/office-cards/BaseCard.tsx
@@ -0,0 +1,38 @@
+import Image from 'next/image';
+
+import Button from '@/components/ui/Button';
+
+const BaseCard: React.FC<{
+  imageSrc: string;
+  name: string;
+  title: string;
+}> = ({ name, title, imageSrc }) => {
+  return (
+    <div className="relative w-[525px] h-[250px] flex items-center bg-blue text-white max-w-3xl rounded-[32.5px] transition-all duration-300 overflow-visible">
+      {/* Officer Image */}
+      <div className="absolute left-0 bottom-0 w-1/2 h-auto transform translate-x-[-0.5px] translate-y-[0%] z-0">
+        <Image
+          src={`/images/samahan-cutouts/${imageSrc}`}
+          alt={name}
+          width={450}
+          height={600}
+          className="object-cover"
+          priority
+        />
+      </div>
+      {/* Text content */}
+      <div className="flex flex-col justify-between ml-auto w-1/2 space-y-2 p-4 z-10 gap-10">
+        <div className="text-left">
+          <h2 className="font-bold text-thin text-left">{name}</h2>
+          <p className="text-main font-normal">{title}</p>
+        </div>
+        {/* Button */}
+        <div className="flex justify-end">
+          <Button text="View Office" colorScheme="white" />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default BaseCard;
