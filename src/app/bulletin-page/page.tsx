7deb7b0423eb460efd90f88be55e8fe3c8b05adb
yashiyoshi <93021275+yashiyoshi@users.noreply.github.com>
2024-10-28 22:37:48 +0800

    chore: moved BulletinPage to proper file

--- src/app/bulletin-page/page.tsx
+++ src/app/bulletin-page/page.tsx
@@ -1,66 +1,12 @@
 'use client';
-import React from 'react';
+import BulletinPage from '@/components/bulletin-page/BulletinPage';
 
-import { Banner } from '@/components/ui/banner/Banner';
-import BulletinCard from '@/components/ui/BulletinCard';
-import { useCategories } from '@/lib/queries/categoriesQueries';
-
-const formatDate = (dateString: string | number | Date) => {
-  const options = {
-    year: 'numeric' as const,
-    month: 'long' as const,
-    day: '2-digit' as const,
-  };
-  return new Date(dateString).toLocaleDateString(undefined, options);
-};
-
-const BulletinPage: React.FC = () => {
-  const { data: categories } = useCategories();
-
+const Bulletin = () => {
   return (
-    <main>
-      <Banner
-        image="/images/office-of-the-president.png"
-        imageAlt="SAMAHAN Members Image"
-        subtitle="SAMAHAN"
-        title="BULLETIN"
-      />
-      <div className="p-12 overflow-x-hidden">
-        <div className="flex flex-col items-center mx-4 md:mx-12">
-          {categories?.map((category) => (
-            <div key={category.id} className="mb-32 w-full">
-              <div className="flex flex-col mb-8">
-                <p className="text-title text-blue font-bold text-center">
-                  {category.name}
-                </p>
-                <p className="text-blue text-center">
-                  {category.description} lorem ipsum lorem ipsum lorem ipsum
-                  lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum
-                  lorem ipsum lorem ipsum
-                </p>
-              </div>
-              <div className="flex justify-center">
-                <div className="flex-row lg:grid grid-cols-1 md:grid-cols-2 gap-x-12">
-                  {category.bulletins.map((bulletin) => (
-                    <div className="w-full mb-12" key={bulletin.id}>
-                      <BulletinCard
-                        author={bulletin.author}
-                        content={bulletin.content}
-                        id={bulletin.id}
-                        pdfAttachments={bulletin.pdfAttachments} // empty for a while till fixed
-                        published_at={formatDate(bulletin.published_at)}
-                        title={bulletin.title}
-                      />
-                    </div>
-                  ))}
-                </div>
-              </div>
-            </div>
-          ))}
-        </div>
-      </div>
-    </main>
+    <div>
+      <BulletinPage />
+    </div>
   );
 };
 
-export default BulletinPage;
+export default Bulletin;
--- src/components/bulletin-page/BulletinPage.tsx
+++ src/components/bulletin-page/BulletinPage.tsx
@@ -1,17 +1,22 @@
+'use client';
+import React from 'react';
+
 import { Banner } from '@/components/ui/banner/Banner';
 import BulletinCard from '@/components/ui/BulletinCard';
-import Footer from '@/components/ui/Footer/Footer';
-// import { Categories } from '@/lib/types/categories.type';
-// import { useCategories } from '@/lib/queries/categoriesQueries';
-// import { useBulletins } from '@/lib/queries/bulletinQueries';
-// import { Bulletin } from '@/lib/types/bulletin.type';
+import { useCategories } from '@/lib/queries/categoriesQueries';
 
-// type CategoriesProps = {
-//   description: string;
-//   name: string;
-// }
+const formatDate = (dateString: string | number | Date) => {
+  const options = {
+    year: 'numeric' as const,
+    month: 'long' as const,
+    day: '2-digit' as const,
+  };
+  return new Date(dateString).toLocaleDateString(undefined, options);
+};
 
-const BulletinPage = () => {
+const BulletinPage: React.FC = () => {
+  const { data: categories } = useCategories();
+
   return (
     <main>
       <Banner
@@ -20,25 +25,40 @@ const BulletinPage = () => {
         subtitle="SAMAHAN"
         title="BULLETIN"
       />
-      <div className="p-12 overflow-x-hidden" />
-      <div className="flex flex-col text-center justify-center mx-10">
-        <div className="mb-32">
-          <h1 className="text-blue font-bold text-title mb-4">BULLETINS</h1>
-          <div className="mt-12 grid grid-cols-2 gap-x-6 gap-y-10">
-            <BulletinCard
-              author={''}
-              content={''}
-              id={0}
-              pdfAttachments={[]}
-              published_at={''}
-              title={''}
-            />
-          </div>
+      <div className="p-12 overflow-x-hidden">
+        <div className="flex flex-col items-center mx-4 md:mx-12">
+          {categories?.map((category) => (
+            <div key={category.id} className="mb-32 w-full">
+              <div className="flex flex-col mb-8">
+                <p className="text-title text-blue font-bold text-center">
+                  {category.name}
+                </p>
+                <p className="text-blue text-center">
+                  {category.description} lorem ipsum lorem ipsum lorem ipsum
+                  lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum
+                  lorem ipsum lorem ipsum
+                </p>
+              </div>
+              <div className="flex justify-center">
+                <div className="flex-row lg:grid grid-cols-1 md:grid-cols-2 gap-x-12">
+                  {category.bulletins.map((bulletin) => (
+                    <div className="w-full mb-12" key={bulletin.id}>
+                      <BulletinCard
+                        author={bulletin.author}
+                        content={bulletin.content}
+                        id={bulletin.id}
+                        pdfAttachments={bulletin.pdfAttachments} // empty for a while till fixed
+                        published_at={formatDate(bulletin.published_at)}
+                        title={bulletin.title}
+                      />
+                    </div>
+                  ))}
+                </div>
+              </div>
+            </div>
+          ))}
         </div>
       </div>
-      <footer>
-        <Footer />
-      </footer>
     </main>
   );
 };
