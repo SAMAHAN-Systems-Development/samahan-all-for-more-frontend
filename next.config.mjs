179c84dcfbf52b5d60bbbee21da59a34fab58d22
Rhaey <rhaeyislyfe@gmail.com>
2024-10-30 22:39:29 +0800

    feat: add deploy config

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-        domains: ['picsum.photos', 'loremflickr.com'], 
-    }
-    
+  images: {
+    domains: ['picsum.photos', 'loremflickr.com'],
+  },
+  output: 'export',
 };
 
 export default nextConfig;
