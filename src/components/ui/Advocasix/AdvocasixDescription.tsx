4aed679ab659902ad935906650c52c97f6c7f95a
seanchvz <seann.chvz@gmail.com>
2024-10-30 20:55:32 +0800

    refactor (AdvocasixModal): Resolved node errors

--- src/components/ui/Advocasix/AdvocasixDescription.tsx
+++ src/components/ui/Advocasix/AdvocasixDescription.tsx
@@ -3,10 +3,10 @@ import React from 'react';
 const AdvocasixDescription = () => {
   return (
     <>
-      <h2 className="text-4xl sm:text-5xl md:text-6xl lg:text-title text-blue font-bold pt-20 px-4 sm:px-10 md:px-16 lg:px-20 text-center md:text-left">
+      <h2 className="text-4xl sm:text-5xl md:text-6xl lg:text-title text-blue font-bold font-inter pt-20 px-4 sm:px-10 md:px-16 lg:px-20 text-center md:text-left">
         ADVOCASIX
       </h2>
-      <p className="text-blue text-main md:text-xl xl:text-2xl pr-20 pl-20 pt-5">
+      <p className="text-blue font-inter text-main md:text-xl xl:text-2xl pr-20 pl-20 pt-5">
         Guided by its principles as a Jesuit, Filipino, and Mindanaoan
         autonomous student government, the Samahan ng mga Mag-aaral ng
         Pamantasan ng Ateneo de Davao set the goal of championing the identified
