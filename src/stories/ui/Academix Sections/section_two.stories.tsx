cbf504260e4fbf11fda4eca20a3ae57c8b89c263
Ninya <mnamparaiso@addu.edu.ph>
2024-10-30 19:14:15 +0800

    fix: run build error

--- src/stories/ui/Academix Sections/section_two.stories.tsx
+++ src/stories/ui/Academix Sections/section_two.stories.tsx
@@ -2,14 +2,6 @@ import type { Meta, StoryObj } from '@storybook/react';
 import ClusterAcademicArms from '@/academix sections/sectionTwo';
 import sections from '@/data/ClusterAcademicArms.json';
 
-// Define the type for each section item
-interface Section {
-  title: string;
-  icon: string;
-  hoverIcon: string;
-  description: string;
-}
-
 const meta: Meta<typeof ClusterAcademicArms> = {
   title: 'Components/ClusterAcademicArms',
   component: ClusterAcademicArms,
@@ -20,5 +12,5 @@ export default meta;
 type Story = StoryObj<typeof ClusterAcademicArms>;
 
 export const Default: Story = {
-  render: () => <ClusterAcademicArms sections={sections as Section[]} />,
+  render: () => <ClusterAcademicArms sections={sections} />,
 };
