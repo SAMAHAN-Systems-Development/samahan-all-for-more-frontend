fb9abc52c1fd4d0145e49c8512bccd9b410a8b50
unknown <paololuisramirez@gmail.com>
2024-11-02 15:54:38 +0800

    refactor: adjusted the osp json and modal

--- src/components/ui/Advocasix Modal/AdvocasixModal.tsx
+++ src/components/ui/Advocasix Modal/AdvocasixModal.tsx
@@ -17,7 +17,7 @@ const AdvocasixModal: React.FC<AdvocasixModalProps> = ({
   onClose,
 }) => {
   return (
-    <div className="fixed inset-0 flex items-center justify-center z-50">
+    <div className="fixed inset-0 flex items-center justify-center z-[100]">
       {' '}
       {/* Center modal */}
       {/* Overlay to darken the background */}
