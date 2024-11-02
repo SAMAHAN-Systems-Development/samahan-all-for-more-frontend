96a28c1d769e9e5abda4988daf3f903d85d8047f
unknown <paololuisramirez@gmail.com>
2024-11-02 18:56:29 +0800

    feat: finalized homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,11 @@
 'use client';
-import AdvocasixCard from '@/components/ui/Advocasix/AdvocasixCard';
-import type { Metadata } from 'next';
-import advocasixCardData from '@/data/advocasixCardData.json';
+
 import HomeSection from '@/components/Sections/HomePage';
 
 export default function Home() {
   return (
     <main>
       {/* add sections here */}
-      {/* <AdvocasixCard data={advocasixCardData} /> */}
       <HomeSection />
     </main>
   );
