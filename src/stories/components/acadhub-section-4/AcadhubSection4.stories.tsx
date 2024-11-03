6a55277bad2a48afc83e6c8356cfcad1308c794d
edgee20 <ejfusingan0@gmail.com>
2024-11-03 18:17:16 +0800

    feat: created section 4 of Academix: Acadhub Page

---
+++ src/stories/components/acadhub-section-4/AcadhubSection4.stories.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import type { Meta, StoryFn } from '@storybook/react';
+
+import AcadhubSection4 from '@/components/acadhub-page-section-4/AcadHubSection4';
+
+export default {
+  title: 'Components/AcadhubSection4',
+  component: AcadhubSection4,
+} as Meta;
+
+export const Default: StoryFn = () => <AcadhubSection4 />;
