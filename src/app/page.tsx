80220b6c9c831d2a8a2149cac5a3df62fce9d111
seanchvz <seann.chvz@gmail.com>
2024-11-04 11:57:58 +0800

    fix: card responsiveness

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 'use client';
 
 import HomeSection from '@/components/Sections/HomePage';
+import AdvocasixCard from '@/components/ui/Advocasix/AdvocasixCard';
 
 export default function Home() {
   return (
