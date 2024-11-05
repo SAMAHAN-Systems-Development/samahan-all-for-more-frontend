7906c1695d64378a91c70090eae90e8112dbd4e3
Ninya <mnamparaiso@addu.edu.ph>
2024-11-05 11:37:17 +0800

    fix: - bug: advocasix card disables clicking of navbar while its opened.

--- src/components/ui/Advocasix Modal/AdvocasixModal.tsx
+++ src/components/ui/Advocasix Modal/AdvocasixModal.tsx
@@ -17,25 +17,29 @@ const AdvocasixModal: React.FC<AdvocasixModalProps> = ({
   onClose,
 }) => {
   return (
-    <div className="fixed inset-0 flex items-center justify-center z-[100]">
-      {' '}
-      {/* Center modal */}
+    <div className="fixed inset-x-0 top-28 sm:top-24 md:top-24 flex items-center justify-center z-40 px-4 sm:px-6">
       {/* Overlay to darken the background */}
-      <div className="absolute inset-0 bg-black bg-opacity-50" />
-      <div className="relative shadow-2xl w-full max-w-[900px] rounded-3xl overflow-hidden mx-auto bg-white">
+      <div
+        className="absolute inset-0 bg-black bg-opacity-50"
+        onClick={onClose}
+      />
+
+      {/* Modal content */}
+      <div className="relative w-full max-w-[95%] sm:max-w-[600px] md:max-w-[700px] lg:max-w-[900px] rounded-3xl overflow-hidden shadow-2xl mx-auto bg-white">
         {/* Header with Cover Image */}
-        <div className="relative h-[400px] sm:h-[300px] md:h-[400px]">
+        <div className="relative h-[250px] sm:h-[300px] md:h-[350px] lg:h-[400px]">
           <Image
             src={`/images/advocasix-pictures/${image}`}
             alt={title}
             className="h-full w-full object-cover"
             width={900}
             height={400}
+            priority
           />
 
           {/* Close button inside modal */}
           <button
-            className="absolute top-4 right-4 text-white text-3xl"
+            className="absolute top-4 right-4 text-white text-3xl sm:text-4xl"
             onClick={onClose}
           >
             <AiOutlineClose />
@@ -43,9 +47,13 @@ const AdvocasixModal: React.FC<AdvocasixModalProps> = ({
         </div>
 
         {/* Main Content */}
-        <div className="p-6 text-blue text-center mb-24">
-          <h2 className="text-2xl sm:text-3xl font-bold mb-4">{title}</h2>
-          <p className=" mx-6 text-sm sm:text-base">{content}</p>
+        <div className="p-4 sm:p-6 text-blue text-center mb-16 sm:mb-20 md:mb-24">
+          <h2 className="text-xl sm:text-2xl md:text-3xl font-bold mb-4">
+            {title}
+          </h2>
+          <p className="text-xs sm:text-sm md:text-base lg:text-lg mx-2 sm:mx-6">
+            {content}
+          </p>
         </div>
       </div>
     </div>
