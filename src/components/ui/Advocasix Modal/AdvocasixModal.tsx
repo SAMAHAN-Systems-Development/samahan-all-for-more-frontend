8be6c53bdbaa09a754d05c73f3286919d916e20c
Ninya <mnamparaiso@addu.edu.ph>
2024-11-05 12:01:38 +0800

    chore: advocasix card will close when clicking outside of the modal.

--- src/components/ui/Advocasix Modal/AdvocasixModal.tsx
+++ src/components/ui/Advocasix Modal/AdvocasixModal.tsx
@@ -17,12 +17,18 @@ const AdvocasixModal: React.FC<AdvocasixModalProps> = ({
   onClose,
 }) => {
   return (
-    <div className="fixed inset-0 flex items-center justify-center z-[100]">
-      {' '}
-      {/* Center modal */}
+    <div
+      className="fixed inset-0 top-28 sm:top-24 md:top-20 flex items-center justify-center z-40 px-4 sm:px-6"
+      onClick={onClose}
+    >
       {/* Overlay to darken the background */}
       <div className="absolute inset-0 bg-black bg-opacity-50" />
-      <div className="relative shadow-2xl w-full max-w-[900px] rounded-3xl overflow-hidden mx-auto bg-white">
+
+      {/* Modal content */}
+      <div
+        className="relative w-full max-w-[95%] sm:max-w-[600px] md:max-w-[700px] lg:max-w-[900px] rounded-3xl overflow-hidden shadow-2xl mx-auto bg-white"
+        onClick={(event) => event.stopPropagation()}
+      >
         {/* Header with Cover Image */}
         <div className="relative h-[400px] sm:h-[300px] md:h-[400px]">
           <Image
