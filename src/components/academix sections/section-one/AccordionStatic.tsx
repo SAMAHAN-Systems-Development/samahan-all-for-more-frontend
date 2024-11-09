c6e731e8c3743227a4dbbfb56d7ceaa3c4d953e7
unknown <paololuisramirez@gmail.com>
2024-11-09 17:41:53 +0800

    fix: adjusted the accordion like component

--- src/components/academix sections/section-one/AccordionStatic.tsx
+++ src/components/academix sections/section-one/AccordionStatic.tsx
@@ -1,5 +1,4 @@
 'use client';
-import { IoIosArrowDropdown } from 'react-icons/io';
 
 interface AccordionProps {
   title: string;
@@ -8,17 +7,13 @@ interface AccordionProps {
 
 const Accordion = ({ title, href }: AccordionProps) => {
   return (
-    <div className="p-[5px_10px_5px_25px] mb-3 border-2 border-blue rounded-[32px] bg-blue transition-colors duration-300">
+    <div className="p-[5px_8px_5px_15px] sm:p-[5px_10px_5px_25px] mb-3 border-2 border-blue rounded-[20px] sm:rounded-[32px] bg-blue transition-colors duration-300">
       <a
         href={href}
-        className="flex justify-between items-center p-2.5 rounded-[35px] text-white text-main leading-[21.78px]"
+        className="flex justify-between items-center p-1.5 sm:p-2.5 rounded-[25px] sm:rounded-[35px] text-white text-sm sm:text-main leading-[18px] sm:leading-[21.78px]"
       >
-        <h3 className="mr-5">{title}</h3>
-        <span className="transition-transform duration-300 size-8">
-          <div className="flex justify-center items-center w-7 h-7 rounded-full border-blue border-2">
-            <IoIosArrowDropdown size={25} />
-          </div>
-        </span>
+        <h3 className="mr-3 sm:mr-5">{title}</h3>
+        <span className="transition-transform duration-300 size-6 sm:size-8"></span>
       </a>
     </div>
   );
